ed03f48514ffb600ed2446d552e4ee350a7f4971
dm-zhuk <dm.zhuk@gmail.com>
2026-05-26 19:04:50 +0300

    Add existing AWS bucket to Terraform’s local state book, test run

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,8 @@ resource "aws_s3_bucket_public_access_block" "security_gate" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+import {
+  to = aws_s3_bucket.sap_lake
+  id = "mcs-capstone-datalake-829703038395"
+}
